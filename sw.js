eddcc9a67a0bbf728fac43dd69e393696264d6c1
enesgogtas <enes.gogtas.156@gmail.com>
2026-08-12 21:27:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,74 +1,52 @@
-const CACHE_NAME = "hasta-takip-v1";
+const CACHE_NAME = "hasta-takip-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
-  "./app.js",
+  "./script.js",
   "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
+// Yeni sürüm dosyalarını cache'e al
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 
   event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then(cache => {
-
-        return cache.addAll(
-          FILES_TO_CACHE
-        );
-
-      })
-
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
-
-  self.skipWaiting();
 });
 
-
-self.addEventListener("activate", event => {
-
+// Eski cache'leri temizle
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-
-    caches.keys()
-      .then(keys => {
-
-        return Promise.all(
-
-          keys
-            .filter(key =>
-              key !== CACHE_NAME
-            )
-            .map(key =>
-              caches.delete(key)
-            )
-
-        );
-
-      })
-
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
   );
 
   self.clients.claim();
 });
 
-
-self.addEventListener("fetch", event => {
-
+// Güncel dosyaları almaya çalış
+self.addEventListener("fetch", (event) => {
   event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
 
-    caches.match(event.request)
-      .then(cachedResponse => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
 
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return fetch(event.request);
-
+        return response;
       })
-
+      .catch(() => caches.match(event.request))
   );
-
 });
